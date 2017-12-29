6403667ddcb217f6e4736f9bef08bc7c094399c1
wang125631 <1256317570@qq.com>
2017-12-29 23:06:39 +0800

    12/29

--- src/com/wpx/demo08/demo07.java
+++ src/com/wpx/demo08/demo07.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.wpx.demo08;
 /**
  * 权限修饰符： 权限修饰符就是控制被修饰的成员的范围可见性。
@@ -33,39 +33,3 @@ public class demo07 {
 		
 	}
 }
-=======
-package com.wpx.demo08;
-/**
- * 权限修饰符： 权限修饰符就是控制被修饰的成员的范围可见性。
-
-			public(公共)        protected(受保护)           default(缺省)       private (大到小)
-
-		同一个类      true                 true                        true                   true
-		
-		
-		同一个包      true                 true                        true                   false
-		
-		  
-		子父类        true                 true                        false                  false
-		
-		
-		不同包        true                 false                       false                  false
-
-
-注意： 在不同包下面只有public 与 protected 可以访问，而且protected必须是在继承关系下才能够访问。
- * @author wangpx
- */
-public class demo07 {
-	
-	int age;
-	
-	private String name;
-	
-	protected void name() {
-		
-	}
-	public void say() {
-		
-	}
-}
->>>>>>> d128562b8b5099957eb34bce2139a4cb6dc7e90e
